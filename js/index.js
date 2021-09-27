13525afbb711e8c32f300af113c5b26e3269a982
zolir <rc.drift18@gmail.com>
2021-09-27 23:44:43 +0200

    add new changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-(function () {
-  var toggle = document.querySelector('.nav-toggle');
-  
-  toggle.addEventListener('click', function(e) {
-    this.classList.toggle('opened');
-  });
-})();
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: '.slider__arrow-left',
+        prevEl: '.slider__arrow-right',
+    }
+
+});
+
+
+const navbarBttn = document.querySelector('.hamburger-menu');
+const bar = document.querySelector('.bar');
+const headerWrap = document.querySelector('.header-wrap');
+navbarBttn.addEventListener('click', navbar)
+
+function navbar() {
+    bar.classList.toggle('animate');
+    headerWrap.classList.toggle('open__nav');
+}
