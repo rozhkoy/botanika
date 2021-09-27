17883f18ed9f6ffed5d80b1a226855f684c4cb59
zolir <rc.drift18@gmail.com>
2021-09-28 00:44:13 +0200

    add new changes

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    navigation: {
-        nextEl: '.slider__arrow-left',
-        prevEl: '.slider__arrow-right',
+window.onload = function() {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        navigation: {
+            nextEl: '.slider__arrow-left',
+            prevEl: '.slider__arrow-right',
+        }
+
+    });
+
+
+    const navbarBttn = document.querySelector('.hamburger-menu');
+    const bar = document.querySelector('.bar');
+    const headerWrap = document.querySelector('.header-wrap');
+    navbarBttn.addEventListener('click', navbar)
+
+    function navbar() {
+        bar.classList.toggle('animate');
+        headerWrap.classList.toggle('open__nav');
     }
-
-});
-
-
-const navbarBttn = document.querySelector('.hamburger-menu');
-const bar = document.querySelector('.bar');
-const headerWrap = document.querySelector('.header-wrap');
-navbarBttn.addEventListener('click', navbar)
-
-function navbar() {
-    bar.classList.toggle('animate');
-    headerWrap.classList.toggle('open__nav');
 }
